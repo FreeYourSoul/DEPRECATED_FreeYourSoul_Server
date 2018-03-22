d46c03664c3d1824343f7b55704a5c7411bb1825
FyS <ballandFyS@gmail.com>
2018-03-23 00:51:48 +0100

    [FYS] [gateway]: - clean warnings

--- Server/test/mock/MockVerifier.hpp
+++ Server/test/mock/MockVerifier.hpp
@@ -9,7 +9,7 @@
 #include <FysBus.hh>
 #include <functional>
 #include <map>
-#include <boost/thread/thread.hpp>
+
 
 namespace FSeam {
 
@@ -124,7 +124,6 @@ namespace FSeam {
         }
 
         static void cleanUp() {
-            boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
             inst = nullptr;
         }
 
